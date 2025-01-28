e0e08a18811adaddeda393df543e6f4cf489cfcc
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2025-01-28 09:24:30 +0100

    test(render_html): TestGenerateTemplateFile

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/mozillazg/go-slugify v0.2.0
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.8.1
+	github.com/stretchr/testify v1.10.0
 )
 
 require (
@@ -49,4 +50,5 @@ require (
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/cvrender/html/html_test.go
+++ internal/cvrender/html/html_test.go
@@ -1,1 +1,185 @@
 package render_html
+
+import (
+	"os"
+	"testing"
+	"text/template"
+
+	"github.com/germainlefebvre4/cvwonder/internal/model"
+	"github.com/stretchr/testify/assert"
+)
+
+var CvTest = model.CV{
+	Company: model.Company{
+		Name: "Company",
+		Logo: "logo.png",
+	},
+	Person: model.Person{
+		Name:        "John Doe",
+		Depiction:   "I am a dummy Software Engineer for test.",
+		Profession:  "Software Engineer",
+		Location:    "Paris",
+		Citizenship: "French",
+		Email:       "john@doe.fr",
+	},
+	SocialNetworks: model.SocialNetworks{
+		Github:        "dummy",
+		Stackoverflow: "dummy",
+	},
+	Abstract: []model.Abstract{
+		{
+			Tr: "I am a dummy Software Engineer for test.",
+		},
+	},
+	Career: model.Career{
+		{
+			CompanyName: "Company",
+			CompanyLogo: "logo.png",
+			Duration:    "2019 - 2020",
+			Missions: []model.Mission{
+				{
+					Position:     "Software Engineer",
+					Company:      "Company",
+					Location:     "Paris",
+					Dates:        "2019 - 2020",
+					Summary:      "I was a Software Engineer.",
+					Technologies: []string{"Go", "Python"},
+					Description:  []string{"I was a Software Engineer."},
+					Project:      "A project for a dummy company.",
+				},
+			},
+		},
+	},
+	TechnicalSkills: model.TechnicalSkills{
+		Domains: []model.Domain{
+			{
+				Name: "Development",
+				Competencies: []model.Competency{
+					{
+						Name:  "Go",
+						Level: 5,
+					},
+				},
+			},
+		},
+	},
+	SideProjects: []model.SideProject{
+		{
+			Name:        "Project",
+			Description: "A project for a dummy company.",
+		},
+	},
+	Certifications: []model.Certification{
+		{
+			CompanyName:       "CompanyName",
+			CertificationName: "CertificationName",
+			Issuer:            "Issuer",
+			Date:              "Date",
+			Link:              "Link",
+			Badge:             "Badge",
+		},
+	},
+	Education: []model.Education{
+		{
+			SchoolName: "SchoolName",
+			SchoolLogo: "SchoolLogo",
+			Degree:     "Degree",
+			Location:   "Location",
+			Dates:      "Dates",
+			Link:       "Link",
+		},
+	},
+}
+
+func NewRenderHTMLServicesTest() RenderHTMLServices {
+	return RenderHTMLServices{}
+}
+
+func TestGetTemplateFunctions(t *testing.T) {
+	type args struct {
+	}
+	tests := []struct {
+		name string
+		args args
+		want template.FuncMap
+	}{
+		{
+			name: "Should return template functions",
+			args: args{},
+			want: template.FuncMap{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.IsType(
+				t,
+				tt.want,
+				getTemplateFunctions(),
+				"getTemplateFunctions()",
+			)
+		})
+	}
+}
+
+func TestGenerateTemplateFile(t *testing.T) {
+	testDirectory, _ := os.Getwd()
+	baseDirectory := testDirectory + "/../../.."
+	type fields struct {
+		RenderHTMLService RenderHTMLServices
+	}
+	type args struct {
+		themeDirectory    string
+		outputDirectory   string
+		outputFilePath    string
+		outputTmpFilePath string
+		cv                model.CV
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr error
+	}{
+		{
+			name:   "Should generate template file",
+			fields: fields{NewRenderHTMLServicesTest()},
+			args: args{
+				themeDirectory:    baseDirectory + "/themes/default",
+				outputDirectory:   baseDirectory + "/generated-test",
+				outputFilePath:    baseDirectory + "/generated-test/cv.html",
+				outputTmpFilePath: baseDirectory + "/generated-test/cv.html.tmp",
+				cv:                CvTest,
+			},
+			wantErr: nil,
+		},
+	}
+	for _, tt := range tests {
+		// Prepare
+		// TODO: Add theme files
+
+		// Run test
+		t.Run(tt.name, func(t *testing.T) {
+			service := NewRenderHTMLServicesTest()
+			assert.Equalf(
+				t,
+				tt.wantErr,
+				service.generateTemplateFile(tt.args.themeDirectory, tt.args.outputDirectory, tt.args.outputFilePath, tt.args.outputTmpFilePath, tt.args.cv),
+				"generateTemplateFile(%v, %v, %v, %v, %v)",
+				tt.args.themeDirectory,
+				tt.args.outputDirectory,
+				tt.args.outputFilePath,
+				tt.args.outputTmpFilePath,
+				tt.args.cv,
+			)
+			assert.DirExists(t, tt.args.outputDirectory)
+			assert.FileExists(t, tt.args.outputFilePath)
+			assert.FileExists(t, tt.args.outputTmpFilePath)
+		})
+
+		// Clean
+		err := os.RemoveAll(tt.args.outputDirectory)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
